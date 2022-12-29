520b8cc6f56590c7805ea7aefa1e1a35e79118ae
viktar33 <viktar.varabei@mail.ru>
2022-12-29 19:55:17 +0300

    Component testing

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,20 @@
+import {CounterComponent} from "./counter.component";
+import {compute} from "../playground/compute";
+
+describe('CounterComponent', () => {
+  let counterComponent:CounterComponent;
+
+  beforeEach( () => {
+    counterComponent = new CounterComponent();
+  })
+
+  it('should increment counter by 1', function () {
+    counterComponent.increment();
+    expect(counterComponent.counter).toBe(1);
+  });
+
+  it('should decrement counter by 1', function () {
+    counterComponent.decrement();
+    expect(counterComponent.counter).toBe(-1);
+  });
+})
