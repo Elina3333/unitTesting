2cc58f9b9d25746adae21b1501e0105f538f538b
viktar33 <viktar.varabei@mail.ru>
2022-12-30 12:24:53 +0300

    Component testing with service

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from "@angular/core";
+import {PostsService} from "./posts.service";
+
+@Component({
+  template: `Posts component`,
+  selector: 'app-posts'
+})
+
+export class PostsComponent implements OnInit {
+  posts = new Array<any>();
+  message: string = '';
+
+  constructor(private service: PostsService) {
+  }
+
+  ngOnInit(): void {
+       this.service.fetch().subscribe(p => {
+         this.posts = p
+       })
+  }
+
+  add(title:string) {
+    const  post = {title};
+    this.service.create(post).subscribe((p) => {
+      this.posts.push(p);
+    }, error => this.message = error)
+  }
+
+  delete(id : number) {
+    if(window.confirm('Are you sure')) {
+      this.service.remove(id).subscribe();
+    }
+  }
+
+}
